d860a5736bc844ff3486accafd17379485d37eea
rainn66 <jaeseong7975@gmail.com>
2023-12-13 17:30:46 +0900

    2023.12.13 study

---
+++ jpashop/src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Rollback(false) //Test 는 롤백해버림
+    public void join() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        Assertions.assertEquals(member, memberRepository.findOne(savedId));
+
+    }
+
+    @Test
+    public void duplicated() throws Exception {
+        //given
+
+
+        //when
+
+
+        //then
+    }
+
+}
