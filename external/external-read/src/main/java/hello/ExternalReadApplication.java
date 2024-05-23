f86b846d853512756e10b755e3ffa6814fbd2866
rainn66 <jaeseong7975@gmail.com>
2024-05-23 20:11:00 +0900

    2024.5.24 study(외부 설정)

---
+++ external/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -0,0 +1,20 @@
+package hello;
+
+import hello.config.MyDataSourceConfigV1;
+import hello.config.MyDataSourceConfigV2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+//@Import(MyDataSourceEnvConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourceConfigV1.class)
+@Import(MyDataSourceConfigV2.class)
+@SpringBootApplication(scanBasePackages = "hello.datasource")
+public class ExternalReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalReadApplication.class, args);
+    }
+
+}
