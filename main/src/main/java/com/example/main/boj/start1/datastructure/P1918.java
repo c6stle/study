add0e9b0c6dd7221f56e46b7fc215a7a4dd6d224
rainn66 <jaeseong7975@gmail.com>
2024-06-12 23:41:53 +0900

    2024.6.12 study 후위표기

--- main/src/main/java/com/example/main/boj/start1/datastructure/P1918.java
+++ main/src/main/java/com/example/main/boj/start1/datastructure/P1918.java
@@ -2,8 +2,6 @@ package com.example.main.boj.start1.datastructure;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.Stack;
 
 //후위표기식
@@ -12,14 +10,39 @@ public class P1918 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String func = br.readLine();
 
-        Queue<Character> val = new ArrayDeque<>();
-        Stack<Character> calc = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
         for (int i = 0; i < func.length(); i++) {
             char c = func.charAt(i);
-            val.offer(c);
-
+            if (c >= 'A' && c <= 'Z') {
+                sb.append(c);
+            } else if (c != '(' && c != ')') {
+                while (!stack.isEmpty() && priority(c) <= priority(stack.peek())) {
+                    sb.append(stack.pop());
+                }
+                stack.push(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else {
+                while (stack.peek() != '(') {
+                    sb.append(stack.pop());
+                }
+                stack.pop(); //여는 괄호 제거
+            }
         }
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        System.out.println(sb);
+    }
 
-
+    public static int priority(char c) {
+        if (c == '*' || c == '/') {
+            return 2;
+        } else if (c == '+' || c == '-') {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
