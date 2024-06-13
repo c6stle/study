ea1462b9e0d8be4f56ba5fa76934481fba7301af
rainn66 <jaeseong7975@gmail.com>
2024-06-13 19:36:53 +0900

    2024.6.13 study 수학

---
+++ main/src/main/java/com/example/main/boj/start1/math/P6588.java
@@ -0,0 +1,18 @@
+package com.example.main.boj.start1.math;
+
+import java.util.Scanner;
+
+//골드바흐의 추측
+public class P6588 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        while ((n = sc.nextInt()) != 0) {
+            int num1 = 1;
+            int num2 = 1;
+
+
+        }
+    }
+}
